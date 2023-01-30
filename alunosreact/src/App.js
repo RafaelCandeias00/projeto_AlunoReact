3ade63ccec8aa348336e3500db91e2e0e0e41a6a
RafaelCandeias00 <rafaelcandeias39@gmail.com>
2023-01-30 16:23:22 -0300

    @RafaelCandeias00 [Task: obtendo dados da API]

--- alunosreact/src/App.js
+++ alunosreact/src/App.js
@@ -1,11 +1,42 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
-import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
+//import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
 
 function App() {
+  // URL para acessar endpoint para obter os alunos a partir da API
+  const baseUrl="https://localhost:5001/api/Alunos"; 
+  
+  // Trata mudança de estado na aplicação relacionada aos dados do aluno
+  const [data, setData]=useState([]); 
+
+  /**
+   * Define o request GET usando o axios com o 
+   * endereço base e obtém a resposta e
+   * usando setData atribui os dados recebidos
+   */
+  const pedidoGet=async()=>{
+    await axios.get(baseUrl)
+    .then(response=>{
+      setData(response.data);
+    }).catch(error=>{
+      console.log(error);
+    })
+  }
+
+  /**
+   * A função setData é resposável por alterar
+   * o estado de data e assim a colocamos
+   * dentro do escopo da função useEffect,
+   * porque ela é a resposável por tratar os dados 
+   * que vão modiciar o estado da aplicação
+   */
+  useEffect(()=>{
+    pedidoGet();
+  })
+
   return (
     <div className="App">
       <br/>
@@ -24,7 +55,18 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          
+          {data.map(aluno=>(
+            <tr key={aluno.id}>
+              <td>{aluno.id}</td>
+              <td>{aluno.nome}</td>
+              <td>{aluno.email}</td>
+              <td>{aluno.idade}</td>
+              <td>
+                <button className='btn btn-primary'>Editar</button> {"  "}
+                <button className='btn btn-danger'>Excluir</button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
