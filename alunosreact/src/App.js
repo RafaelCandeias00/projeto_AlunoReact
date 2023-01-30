f3f5d1071ae407f012fbb0f03081b9d059c2ba72
RafaelCandeias00 <rafaelcandeias39@gmail.com>
2023-01-30 15:38:43 -0300

    @RafaelCandeias00 [Task: definindo esqueleto da página]

--- alunosreact/src/App.js
+++ alunosreact/src/App.js
@@ -1,11 +1,32 @@
 import './App.css';
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Rafael Candeias</h1>
+      <br/>
+      <h3>Cadastro de Alunos</h3>
+      <header>
+        <button className="btn btn-success">Incluir Novo Aluno</button>
       </header>
+      <table className='table table-bordered'>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Idade</th>
+            <th>Operação</th>
+          </tr>
+        </thead>
+        <tbody>
+          
+        </tbody>
+      </table>
     </div>
   );
 }
