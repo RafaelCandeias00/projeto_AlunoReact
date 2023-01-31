5b50ae3d6f04f707a6b6224b133889c69174b777
RafaelCandeias00 <rafaelcandeias39@gmail.com>
2023-01-31 14:30:20 -0300

    @RafaelCandeias00 [Task: add Modal e método POST]

--- alunosreact/src/App.js
+++ alunosreact/src/App.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
-//import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
+import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
 
 function App() {
   // URL para acessar endpoint para obter os alunos a partir da API
@@ -11,6 +11,39 @@ function App() {
   
   // Trata mudança de estado na aplicação relacionada aos dados do aluno
   const [data, setData]=useState([]); 
+
+  // Definir um estado para controlar a janela Modal
+  const [modalIncluir, setModalIncluir]=useState(false);
+
+  // Cria o estado alunoSelecionado
+  const [alunoSelecionado, setAlunoSelecionado]=useState({
+    alunoId: '',
+    nome: '',
+    email: '',
+    idade: ''
+  })
+
+  //Criar método abrirFecharModalIncluir para alterar entre os estados aberto e fechado
+  const abrirFecharModalIncluir=()=>{
+    setModalIncluir(!modalIncluir);
+  }
+
+  /**
+   * Criar o método handleChange para
+   * guardar os dados do aluno que serão
+   * informados no inputs do formulário: 
+   * id, nome, email e idade
+   * 
+   * E usa setAlunoSelecionado para atualizar o estado
+   */
+  const handleChange=e=>{
+    const {name, value}=e.target;
+    setAlunoSelecionado({
+      ...alunoSelecionado,
+      [name]: value
+    });
+    console.log(alunoSelecionado);
+  }
 
   /**
    * Define o request GET usando o axios com o 
@@ -26,6 +59,18 @@ function App() {
     })
   }
 
+  const pedidoPost=async()=>{
+    delete alunoSelecionado.alunoId;
+    alunoSelecionado.idade=parseInt(alunoSelecionado.idade);
+      await axios.post(baseUrl, alunoSelecionado)
+      .then(response=>{
+        setData(data.concat(response.data));
+        abrirFecharModalIncluir();
+      }).catch(error=>{
+        console.log(error);
+      })
+  }
+
   /**
    * A função setData é resposável por alterar
    * o estado de data e assim a colocamos
@@ -38,11 +83,11 @@ function App() {
   })
 
   return (
-    <div className="App">
+    <div className="aluno-container">
       <br/>
       <h3>Cadastro de Alunos</h3>
       <header>
-        <button className="btn btn-success">Incluir Novo Aluno</button>
+        <button onClick={()=>abrirFecharModalIncluir()} className="btn btn-success">Incluir Novo Aluno</button>
       </header>
       <table className='table table-bordered'>
         <thead>
@@ -57,7 +102,7 @@ function App() {
         <tbody>
           {data.map(aluno=>(
             <tr key={aluno.id}>
-              <td>{aluno.id}</td>
+              <td>{aluno.alunoId}</td>
               <td>{aluno.nome}</td>
               <td>{aluno.email}</td>
               <td>{aluno.idade}</td>
@@ -69,6 +114,30 @@ function App() {
           ))}
         </tbody>
       </table>
+      
+      <Modal isOpen={modalIncluir}>
+        <ModalHeader>Incluir Alunos</ModalHeader>
+        <ModalBody>
+          <div className='form-group'>
+            <label>Nome: </label>
+            <br/>
+            <input type='text' className="form-control" name='nome' onChange={handleChange}/>
+            <br/>
+            <label>Email: </label>
+            <br/>
+            <input type='text' className="form-control" name='email' onChange={handleChange}/>
+            <br/>
+            <label>Idade: </label>
+            <br/>
+            <input type='text' className="form-control" name='idade' onChange={handleChange}/>
+            <br/>
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <button className='btn btn-primary' onClick={()=>pedidoPost()}>Incluir</button>{"   "}
+          <button className='btn btn-danger' onClick={()=>abrirFecharModalIncluir()}>Cancelar</button>
+        </ModalFooter>
+      </Modal>
     </div>
   );
 }
